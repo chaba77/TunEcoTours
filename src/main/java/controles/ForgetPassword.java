42a9caa84e9c6e1193e898aca0c94ebd5003f773
mohamed aziz ennour <mohamedaziz.ennour@esprit.com>
2024-04-29 06:57:59 +0100

    Your commit message

---
+++ src/main/java/controles/ForgetPassword.java
@@ -0,0 +1,2 @@
+package controles;public class ForgetPassword {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,2 @@
-module com.example.tunecotours {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-    requires java.sql;
-    requires bcrypt;
-    requires com.google.gson;
-    requires java.desktop;
-
-    opens Test to javafx.fxml, javafx.graphics;
-    exports controles to javafx.fxml;
-    opens controles to javafx.fxml;
-    opens Enntities to javafx.base;
-    exports com.example.tunecotours;
+module $MODULE_NAME$ {
 }
